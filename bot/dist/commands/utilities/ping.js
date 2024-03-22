2c9aee9d6569dde4859a713f382856c3823fbd74
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-03-22 23:50:19 +0900

    fix somethinf

--- bot/dist/commands/utilities/ping.js
+++ bot/dist/commands/utilities/ping.js
@@ -9,10 +9,16 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const types_1 = require("../../types/");
 dotenv_1.default.config();
 const authUrl = (0, types_1.checkIsString)(process.env.AUTHURL);
+const guildId = (0, types_1.checkIsString)(process.env.GUILDID);
 const ping = {
     data: new discord_js_1.SlashCommandBuilder().setName('ping').setDescription('Sample slash command.'),
     async execute(interaction) {
-        interaction.reply(`[OAuth URL](${authUrl})`);
+        if (guildId === interaction.guild?.id) {
+            await interaction.reply(`[OAuth URL](${authUrl})`);
+        }
+        else {
+            await interaction.reply('You are not guild member');
+        }
     },
 };
 exports.ping = ping;
